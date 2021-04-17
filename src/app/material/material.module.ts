13ab4f417b0007a5dae49202f9b87741b6564903
Aaron Chacon <aaronchacon86@gmail.com>
2021-04-17 18:17:09 -0400

    angular material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -27,6 +27,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -44,7 +45,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
 import { OverlayModule } from '@angular/cdk/overlay';
-
 
 
 @NgModule({ 
@@ -95,6 +95,7 @@ import { OverlayModule } from '@angular/cdk/overlay';
     OverlayModule,
     PortalModule,
     ScrollingModule,
+    MatFormFieldModule
   ],
   exports: [
     A11yModule,
@@ -141,6 +142,7 @@ import { OverlayModule } from '@angular/cdk/overlay';
     OverlayModule,
     PortalModule,
     ScrollingModule,
+    MatFormFieldModule
   ]
  })
 export class MaterialModule {   }
--- src/app/pages/admin/components/basic-form/basic-form.component.ts
+++ src/app/pages/admin/components/basic-form/basic-form.component.ts
@@ -17,11 +17,11 @@ export class BasicFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.buildForm();
-    this.form.valueChanges
+    /* this.form.valueChanges
         .subscribe(value => {
           console.log(value);
           
-        })
+        }) */
   }
 
   getNameValue(){
@@ -41,12 +41,22 @@ export class BasicFormComponent implements OnInit {
 
   private buildForm(){
 
+    const today = new Date();
+    const month = today.getMonth();
+    const year = today.getFullYear();
+
     this.form = this.formBuilder.group({
-      name: ['aaronch', [Validators.required, Validators.maxLength(10), Validators.pattern(/^([Aa-zA-ZáéíóúÁÉÍÓÚÑñ]{2,}\s?){2,4}$/) ] ],
+      fullname: this.formBuilder.group({
+        name: ['aaronch', [Validators.required, Validators.maxLength(10), Validators.pattern(/^([Aa-zA-ZáéíóúÁÉÍÓÚÑñ]{2,}\s?){2,4}$/) ] ],
+        last: ['frontend', [Validators.required, Validators.maxLength(10), Validators.pattern(/^([Aa-zA-ZáéíóúÁÉÍÓÚÑñ]{2,}\s?){2,4}$/) ] ],
+      }),
       email: ['mail@aaronch.com', [Validators.required, Validators.email]],
       phone: ['920880481', [Validators.required]],
       color: ['#000000'],
-      date: ['08-04-91'],
+      date: this.formBuilder.group({
+        start: [new Date(year, month, 15)],
+        end: [new Date(year, month, 15)],
+      }),
       number: [29, [Validators.required, Validators.min(18), Validators.max(120)]],
       categoty: ['category-2'],
       //tag: [''],
@@ -60,14 +70,27 @@ export class BasicFormComponent implements OnInit {
   /// name
 
   get nameField() {
-    return this.form.get('name');
+    return this.form.get('fullname').get('name');
   }
 
   get isNameValid() {
-    return this.form.get('name').touched && this.form.get('name').valid;
+    return this.form.get('fullname').get('name').touched && this.form.get('fullname').get('name').valid;
   }
   get isNameInValid() {
-    return this.form.get('name').touched && this.form.get('name').invalid;
+    return this.form.get('fullname').get('name').touched && this.form.get('fullname').get('name').invalid;
+  }
+
+  /// last
+
+  get lastField() {
+    return this.form.get('fullname').get('last');
+  }
+
+  get isLastValid() {
+    return this.form.get('fullname').get('last').touched && this.form.get('fullname').get('last').valid;
+  }
+  get isLastInValid() {
+    return this.form.get('fullname').get('last').touched && this.form.get('fullname').get('last').invalid;
   }
 
   /// Email
