49fc0d15564bd04c35bfdf59aed792c0b532ba00
Aaron Chacon <aaronchacon86@gmail.com>
2021-04-17 13:45:33 -0400

    validators

--- src/app/pages/admin/components/basic-form/basic-form.component.ts
+++ src/app/pages/admin/components/basic-form/basic-form.component.ts
@@ -30,6 +30,8 @@ export class BasicFormComponent implements OnInit {
 
   save(event: Event){
     event.preventDefault();
+    console.log(this.form.valid);
+    
     if (this.form.valid) {
       console.log(this.form.value);
     } else {
@@ -40,20 +42,22 @@ export class BasicFormComponent implements OnInit {
   private buildForm(){
 
     this.form = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.maxLength(10)] ],
-      enail: [''],
-      phone: [''],
+      name: ['aaronch', [Validators.required, Validators.maxLength(10), Validators.pattern(/^([Aa-zA-ZáéíóúÁÉÍÓÚÑñ]{2,}\s?){2,4}$/) ] ],
+      email: ['mail@aaronch.com', [Validators.required, Validators.email]],
+      phone: ['920880481', [Validators.required]],
       color: ['#000000'],
-      date: [''],
-      number: [12],
-      categoty: [''],
-      tag: [''],
-      agree: [false],
-      gender: [''],
-      zone: [''],
+      date: ['08-04-91'],
+      number: [29, [Validators.required, Validators.min(18), Validators.max(120)]],
+      categoty: ['category-2'],
+      //tag: [''],
+      agree: [true, [Validators.requiredTrue]],
+      //gender: [''],
+      //zone: [''],
     });
 
   }
+
+  /// name
 
   get nameField() {
     return this.form.get('name');
@@ -65,5 +69,32 @@ export class BasicFormComponent implements OnInit {
   get isNameInValid() {
     return this.form.get('name').touched && this.form.get('name').invalid;
   }
+
+  /// Email
+
+  get emailField() {
+    return this.form.get('email');
+  }
+
+  get isEmailValid() {
+    return this.form.get('email').touched && this.form.get('email').valid;
+  }
+  get isEmailInValid() {
+    return this.form.get('email').touched && this.form.get('email').invalid;
+  }
+
+  /// phone
+  
+  get phoneField() {
+    return this.form.get('phone');
+  }
+
+  get isPhoneValid() {
+    return this.form.get('phone').touched && this.form.get('phone').valid;
+  }
+  get isPhoneInValid() {
+    return this.form.get('phone').touched && this.form.get('phone').invalid;
+  }
+  
 
 }
