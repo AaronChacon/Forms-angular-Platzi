07235f1082270a83b1fbe6268523355d23ca32e1
Aaron Chacon <aaronchacon86@gmail.com>
2021-04-18 12:45:54 -0400

    conditional validations

--- src/app/pages/auth/components/register/register.component.ts
+++ src/app/pages/auth/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Event, Router } from '@angular/router';
 import { AuthService } from '../../../../core/services/auth.service';
+import { MyValidators } from '../../../../utils/validators';
 
 @Component({
   selector: 'app-register',
@@ -23,11 +24,35 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get typeField(){
+    return this.form.get('type')
+  }
+  
+  get companyNameField(){
+    return this.form.get('companyName')
+  }
+
   private buildForm ()  {
     this.form = this.fb.group({
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required]],
-    })
+      email: ['mail@aaronch.com', [Validators.required]],
+      password: ['1234567890', [Validators.required, Validators.minLength(6), MyValidators.validPassword,]],
+      confirmPassword: ['1234567890', [Validators.required,]],
+      type: ['company', [Validators.required]],
+      companyName: ['', [Validators.required]],
+    },{
+      validators: MyValidators.matchPassword
+    });
+
+    this.typeField.valueChanges
+        .subscribe(value => {
+          console.log(value);
+          if(value === 'company') {
+            this.companyNameField.setValidators([Validators.required])
+          } else {
+            this.companyNameField.setValidators(null)
+          }
+          this.companyNameField.updateValueAndValidity();
+        })
   }
 
   register() {
--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -1,4 +1,5 @@
 import { AbstractControl } from "@angular/forms";
+import { Controller } from "swiper";
 
 export class MyValidators {
 
@@ -13,4 +14,31 @@ export class MyValidators {
 
     }
 
+    static validPassword(control: AbstractControl){
+        const value = control.value;
+
+        if (!containsNumber(value)) {
+            return {invalid_password: true};
+        }
+        return  null
+    }
+
+    static matchPassword(control: AbstractControl){
+        const password = control.get('password').value;
+        const confirmPassword = control.get('confirmPassword').value;
+        if (password === confirmPassword) {
+            return null
+        }
+        return {invalid_match_pass: true};
+    }
+
+    
+}
+
+function containsNumber(value: string) {
+    return value.split('').find(value => isNumber(value)) !== undefined;
+}
+
+function isNumber(value: string) {    
+    return !isNaN(parseInt(value, 10));
 }
