c003af74115a705daba33e460b5646e0020e2e9c
Aaron Chacon <aaronchacon86@gmail.com>
2021-04-20 02:05:03 -0400

    dynamic form

--- src/app/pages/order/components/order/order.component.ts
+++ src/app/pages/order/components/order/order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../../../../product.model';
 import { CartService } from '../../../../core/services/cart.service';
 import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-order',
@@ -12,16 +13,45 @@ export class OrderComponent implements OnInit {
 
   products$: Observable<IProduct[]>
 
+  form: FormGroup;
+
   constructor(
     private cartService: CartService,
+    private formBuilder: FormBuilder, 
   ) {
-
-    this.products$ = this.cartService.cart$
-    
+    this.products$ = this.cartService.cart$;
+    this.buildForm();
   }
 
   ngOnInit(): void {
 
+  }
+
+  private buildForm(){
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      address: this.formBuilder.array([])
+    })
+  }
+
+  addAddressField(){
+    this.addressField.push(this.createAddressField());
+  }
+
+  private createAddressField(){
+    return this.formBuilder.group({
+      zip: ['', [Validators.required]],
+      text: ['', [Validators.required]],
+    })
+  }
+
+  get addressField(){
+    return this.form.get('address') as FormArray;
+  }
+
+  saveData(event){
+    event.preventDefault();
+    console.log(this.form.value);
   }
 
 }
